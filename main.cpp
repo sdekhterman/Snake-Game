a23ea8d0e04116863648488805d8fbed98a4e0ea
sdekhterman <sdekhterman@gmail.com>
2025-02-23 15:17:02 -0600

    readability reorder and comments
    main is the first function to be read and that should help people understand the structure of the program better. Some comments now also explain why features were implmented the way they were.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@
 #include<conio.h>
 using namespace std;
 
+// TODO: add the sleep duration and player/ boarder chars, also check if the var inits should go in main for garbage collectuin reasons
 
+// Set the play area height and width, chosen to be square arbirary. 
+// The width is twice the height since chars are about twice as tall as they are wide ...
+// and the player moves two chares to the side for each char up/down to make movment more consistant.
+// The score incrment is used to indicates to the player that a given action, collecting fruit, is desired as it increase the score variable which is printed to the player. 
 const int height         = 20;
 const int width          = 2*height;
 const int scoreIncrement = 10;
 
+// Initialize size of tail, set to -1 since a function will set it to 0 on startup and allows for function reuse, ...
+// a food location, the tail array, and that the game is not over, since it is just starting.
 int ntail      = -1;
 int foodx      = 1;
 int foody      = 1;
@@ -15,13 +22,41 @@ int tailx[100] = {};
 int taily[100] = {};
 bool gameover  = false;
 
+// Setup game functions so main is understood first when reading code
+void update();
+void setup();
+void input();
+void logic();
+void draw();
+
+// Stores player diection
 enum edirection { STOP = 0, UP, DOWN, LEFT, RIGHT };
 edirection dir;
 
+// Initialize the player to start in the center of the play area, so they can get to any first fruit on average at the same speed initially, ...
+// and the score, -scoreIncrement initially as it will be set to 0 on startup and allows for function reuse. 
 int x     = width  / 2;
 int y     = height / 2;
 int score = -scoreIncrement;
 
+int main()
+{
+    setup(); // Initialize the game
+    
+    while (!gameover)
+    {
+        input();
+        logic();
+        draw();
+        Sleep(120); // Sleep duration
+    }
+
+    cout << "GAME OVER" << endl;
+
+    return 0;
+}
+
+// Function to update the score, spawn new food, and increase the tail length number
 void update() {
     score += 10;
     do {
@@ -36,18 +71,116 @@ void update() {
 // Function to initialize the game state
 void setup()
 {  
-    srand(time(NULL)); // used to set the food spawn location at run time instead of compile time (so the food spawn changes every game)
+    srand(time(NULL)); // Used to set the food spawn location at run time instead of compile time (so the food spawn changes every game)
     update();
-    system("CLS"); // send out a clear screen command to the virtial terminal to not have the previous commands appear outside the boarder of the play area
+    system("CLS"); // Send out a clear screen command to the virtial terminal to not have the previous commands appear outside the boarder of the play area
 }
-// TODO: only have the changes chars update
+
+// Function to handle player input
+void input()
+{
+    if (_kbhit())
+    {
+        char key = _getch();
+        switch (key)
+        {
+        case 'w':
+        case 'W':
+        case 72: // ASCII code for up arrow key
+            if(dir!=DOWN)
+            {
+                dir = UP;
+            }
+            break;
+        case 's':
+        case 'S':
+        case 80: // ASCII code for down arrow key
+            if(dir!=UP)
+            {
+                dir = DOWN;
+            }
+            break;
+        case 'a':
+        case 'A':
+        case 75: // ASCII code for left arrow key
+            if(dir!=RIGHT)
+            {
+                dir = LEFT;
+            }
+            break;
+        case 'd':
+        case 'D':
+        case 77: // ASCII code for right arrow key
+            if(dir!=LEFT)
+            {
+                dir = RIGHT;
+            }
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+// Function to update game logic
+void logic()
+{
+    // Update tail positions
+    for (int i = ntail - 1; i > 0; --i)
+    {
+        tailx[i] = tailx[i - 1];
+        taily[i] = taily[i - 1];
+    }
+
+    // Move the head to the new position
+    tailx[0] = x;
+    taily[0] = y;
+
+    // Move the head based on the direction
+    switch (dir)
+    {
+        case UP:
+            y--;  break;
+        case DOWN:
+            y++;  break;
+        case LEFT:
+            x-=2; break;
+        case RIGHT:
+            x+=2; break;
+        default:
+            break;
+    }
+
+    // Check for collisions with the borders
+    if (x <= 0 || x >= width || y < 0 || y > height)
+    {
+        gameover = true;
+    }
+
+    // Check for collisions with the tail
+    for (int i = 0; i < ntail; i++)
+    {
+        if (y == taily[i]){
+            if ((x == tailx[i]) || ((dir == LEFT) && ((x+1) == tailx[i])) || ((dir == RIGHT) && ((x-1) == tailx[i]))) {
+                gameover = true;
+            }
+        }
+    }
+
+    // Check for collision with food and update score
+    if (y == foody){
+        if ((x == foodx) || ((dir == LEFT) && ((x+1) == foodx)) || ((dir == RIGHT) && ((x-1) == foodx))) {
+            update();
+        }
+    }
+}
 
 // Function to draw the game screen
 void draw()
 {
-    // Get a handler for the consoles and set the cursor position in a fixed location 
-    // So, the game is redraws over the same place. 
-    // Otherwise, new drawings will be made  every refresh at new terminal lines and the drawings 'char' continunuty will not be maintained.
+    // Get a handler for the console and set the cursor position in a fixed location 
+    // So, the game is redrawn over the same place. 
+    // Otherwise, new drawings will be made  every refresh at new terminal line and the drawings 'char' continunuty will not be maintained.
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), {0,0});
     
     // Set the cursur to be invisable. 
@@ -58,6 +191,7 @@ void draw()
     info.bVisible = false;
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
 
+    // TODO: only have the changes chars update
     // Draw top border
     for (int i = 0; i <= width; i++)
     {
@@ -116,121 +250,4 @@ void draw()
 
     // Display the score
     cout << "Score: " << score << endl;
-}
-
-// Function to handle player input
-void input()
-{
-    if (_kbhit())
-    {
-        char key = _getch();
-        switch (key)
-        {
-        case 'w':
-        case 'W':
-        case 72: // ASCII code for up arrow key
-            if(dir!=DOWN)
-            {
-                dir = UP;
-            }
-            break;
-        case 's':
-        case 'S':
-        case 80: // ASCII code for down arrow key
-            if(dir!=UP)
-            {
-                dir = DOWN;
-            }
-            break;
-        case 'a':
-        case 'A':
-        case 75: // ASCII code for left arrow key
-            if(dir!=RIGHT)
-            {
-                dir = LEFT;
-            }
-            break;
-        case 'd':
-        case 'D':
-        case 77: // ASCII code for right arrow key
-            if(dir!=LEFT)
-            {
-                dir = RIGHT;
-            }
-            break;
-        default:
-            break;
-        }
-    }
-}
-
-
-// Function to update game logic
-void logic()
-{
-    // Update tail positions
-    for (int i = ntail - 1; i > 0; --i)
-    {
-        tailx[i] = tailx[i - 1];
-        taily[i] = taily[i - 1];
-    }
-
-    // Move the head to the new position
-    tailx[0] = x;
-    taily[0] = y;
-
-    // Move the head based on the direction
-    switch (dir)
-    {
-        case UP:
-            y--;  break;
-        case DOWN:
-            y++;  break;
-        case LEFT:
-            x-=2; break;
-        case RIGHT:
-            x+=2; break;
-        default:
-            break;
-    }
-
-    // Check for collisions with the borders
-    if (x <= 0 || x >= width || y < 0 || y > height)
-    {
-        gameover = true;
-    }
-
-    // Check for collisions with the tail
-    for (int i = 0; i < ntail; i++)
-    {
-        if (y == taily[i]){
-            if ((x == tailx[i]) || ((dir == LEFT) && ((x+1) == tailx[i])) || ((dir == RIGHT) && ((x-1) == tailx[i]))) {
-                gameover = true;
-            }
-        }
-    }
-
-    // Check for collision with food and update score
-    if (y == foody){
-        if ((x == foodx) || ((dir == LEFT) && ((x+1) == foodx)) || ((dir == RIGHT) && ((x-1) == foodx))) {
-            update();
-        }
-    }
-}
-
-int main()
-{
-    setup(); // Initialize the game
-    
-    while (!gameover)
-    {
-        input();
-        logic();
-        draw();
-        Sleep(120); // Adjust sleep duration based on mode
-    }
-
-    cout << "GAME OVER" << endl;
-
-    return 0;
 }
